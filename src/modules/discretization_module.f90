c4e6f1502693cbbbc5745d20cdbdc3c59bcdff4d
antonio pucciarelli <antonio.pucc@gmail.com>
2020-09-05 17:44:20 +0200

    Adding CMake source file for the compilation services

--- src/modules/discretization_module.f90
+++ src/modules/discretization_module.f90
@@ -1,10 +1,3 @@
-module math_module
-    implicit none
-
-    real(kind=8), parameter :: pi = 4 * atan(1.0)  
-
-end module math_module
-
 module discretization_module
 
     contains
---
+++ src/modules/math_module.f90
@@ -0,0 +1,6 @@
+module math_module
+    implicit none
+
+    real(kind=8), parameter :: pi = 4 * atan(1.0)  
+
+end module math_module
